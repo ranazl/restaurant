f3fb50d45499f556f6f0a360f73f5ee097672ba0
ranazl <rana.zolfaghari@gmail.com>
2024-12-18 13:34:23 +0330

    comple recipe

--- src/pages/Recipe/Recipe.js
+++ src/pages/Recipe/Recipe.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import { useParams } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
 import './Recipe.css'
 
 export default function Recipe() {
+  const { id } = useParams()
+  const url = 'http://localhost:3000/recipes/' + id
+  const { data: recipe, isLoading, error } = useFetch(url)
+
   return (
-    <div>
-      Recipe
+    <div className='recipe'>
+      {error && <p className='error'>{error}</p>}
+      {isLoading && <p className='loading'>Loading...</p>}
+      {recipe && (
+        <>
+          <h2 className='page-title'>{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook</p>
+          <ul>
+            {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
+          </ul>
+          <p className='method'>{recipe.method}</p>
+        </>
+      )}
     </div>
   )
 }
